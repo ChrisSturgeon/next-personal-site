ac1b96d0b5b3accf40483513ce662f3a1222670a
csturgeon <sturgeon.chris@gmail.com>
2023-04-11 15:17:22 +0100

    Add pop up animation to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,23 @@ import HeadShot from '../imgs/headshot-modified.png';
 
 const inter = Inter({ subsets: ['latin'] });
 
+const greetingVariants = {
+  offscreen: {
+    opacity: 0,
+    y: 50,
+  },
+  onscreen: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: 'spring',
+      bounce: 0.4,
+      duration: 0.5,
+      delay: 0.9,
+    },
+  },
+};
+
 export default function Home() {
   return (
     <>
@@ -20,18 +37,24 @@ export default function Home() {
       <AnimatePresence>
         <motion.div className={styles.wrapper} initial={true}>
           <div className={styles.content}>
-            <div>
-              <motion.h1 transition={{ delay: 0.9 }} animate={{ opacity: 1 }}>
-                Hello!
-              </motion.h1>
+            <motion.div
+              variants={greetingVariants}
+              initial="offscreen"
+              animate="onscreen"
+            >
+              <motion.h1>Hello!</motion.h1>
               <p>I'm Chris Sturgeon. A front end developer from the UK.</p>
-            </div>
-            <nav>
+            </motion.div>
+            <motion.nav
+              variants={greetingVariants}
+              initial="offscreen"
+              animate="onscreen"
+            >
               <Link href="/about">About</Link>
               <Link href="/projects">Projects</Link>
               <Link href="/blog">Blog</Link>
               <Link href="/contact">Contact</Link>
-            </nav>
+            </motion.nav>
             <div className={styles.headshotWrapper}>
               <Image
                 src={'/imgs/headshot-modified.png'}
